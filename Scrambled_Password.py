54ff30dc50161060f3e0fe3553da51a1d2812b9c
TvHead9752-oss <blaketheprofessor@gmail.com>
2024-12-30 09:22:03 -0500

    Update Scrambled_Password.py

--- Scrambled_Password.py
+++ Scrambled_Password.py
@@ -1,11 +1,9 @@
-
 def read_file(file_path):
     with open(file_path, 'r') as file:
         return file.read()
 
 # Extract the username and password
 def extract_credentials(content):
-  
     username_start = content.find('u')
     username_end = content.find('r', username_start) + 1
     username = content[username_start:username_end]
@@ -18,7 +16,12 @@ def extract_credentials(content):
 
     return username, password
 
-# Main login function
+
+def write_creds(username, password):
+    with open("revealed_passcode.txt", "w") as file:
+        file.write(f"Username: {username}\nPassword: {password}\n")
+
+# Main login
 def login():
     file_path = "scrambled_passcode.txt"
     content = read_file(file_path)
@@ -26,23 +29,24 @@ def login():
     username, password = extract_credentials(content)
 
     if not username or not password:
-        # Write fails, now what
         with open("logfile.log", "w") as log_file:
             log_file.write("FAILURE | Username: " + (username or "N/A") + " | Password: " + (password or "N/A") + "\n")
         return
 
+    #  revealed_passcode.txt
+    write_creds(username, password)
+    
     while True:
         input_username = input("Username: ")
         input_password = input("Password: ")
 
         if input_username == username and input_password == password:
-            # Write success
+            # Write success log
             with open("logfile.log", "w") as log_file:
                 log_file.write("SUCCESS | Username: " + input_username + " | Password: " + input_password + "\n")
             print("Login successful!") 
             break
         else:
-        
             print("Invalid username or password. Please try again.")
 
 # Run the login
